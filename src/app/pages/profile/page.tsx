71d81d3cbd6bfa36bde3a234522675cc21280885
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 17:17:04 -0300

    ✨feat: implement Profile page with user data editing functionality and name joining utility

---
+++ src/app/pages/profile/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { NavBar } from "@/components/molecules/Navbar"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useUser } from "@/context/UserContext"
+import { updateUser } from "@/service/UserService"
+import { joinNames } from "@/utils/NameUtils"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+
+function Profile() {
+    const router = useRouter()
+    const { user, refreshUserData } = useUser()
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        try {
+            if (user) {
+                const fullName = joinNames(firstName, lastName)
+
+                const response = await updateUser(user.id, fullName)
+                console.log(response)
+
+                refreshUserData()
+
+                alert("Dados Atualizados!")
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <div>
+            <NavBar />
+
+            <form onSubmit={handleSubmit} >
+                <header className="flex justify-between">
+                    <h1>Editar Perfil</h1>
+
+                    <div className="flex">
+                        <Button variant="destructive" onClick={() => router.back()} className="mr-3">Cancelar</Button>
+                        <Button variant="default" type="submit">Salvar</Button>
+                    </div>
+                </header>
+
+                <main>
+                    <Label>Nome</Label>
+                    <Input
+                        placeholder="Digite seu nome"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                    ></Input>
+
+                    <Label>Sobrenome</Label>
+                    <Input
+                        placeholder="Digite seu sobrenome"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                    ></Input>
+                </main>
+            </form>
+        </div>
+    )
+}
+
+export default Profile
