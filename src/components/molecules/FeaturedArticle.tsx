cc6a253c2615756f50d1882a18b9b6248cf56c3c
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 17:16:43 -0300

    ✨feat: enhance FeaturedArticle component with customizable display options for title, content, author, image, and hover effect

--- src/components/molecules/FeaturedArticle.tsx
+++ src/components/molecules/FeaturedArticle.tsx
@@ -1,39 +1,52 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 import { IArticle } from "@/interface/IArticle";
+import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+
+import { useImageUrl } from "@/hooks/useImageUrl";
+
 
 type Props = {
   article: IArticle;
+  title: boolean
+  content: boolean
+  author: boolean
+  image: boolean
+  hoverEffect: boolean
 };
 
-export const FeaturedArticle = ({ article }: Props) => {
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+export const FeaturedArticle = ({ article, title, content, author, image, hoverEffect }: Props) => {
+  const router = useRouter();
 
-  useEffect(() => {
-    if (article?.image?.data) {
-      const byteArray = new Uint8Array(article.image.data);
-      const blob = new Blob([byteArray], { type: "image/png" });
-      const url = URL.createObjectURL(blob);
-      setImageUrl(url);
+  const imageUrl = useImageUrl(article.image?.data, image)
 
-      return () => URL.revokeObjectURL(url);
-    }
-  }, [article]);
+  const handleClick = () => {
+    router.push(`/pages/article/${article.id}`); //redireciona o usuário ao artigo clicado via id = "pages/article/[id]"
+  };
 
   return (
-    <div className="pr-3">
-      {imageUrl && (
-        <Image src={imageUrl} alt="Imagem do artigo" width={500} height={500} />
-      )}
-      <div className="pb-3">{article.title}</div>
-      <div className="flex justify-around">
-        <div className="flex justify-around">
-          por {article.author?.name},{" "}
-          {new Date(article.releaseDate).toLocaleDateString()}
+    <div className={`transition-colors duration-300 rounded-lg 
+  ${hoverEffect ? "hover:bg-gray-700 cursor-pointer" : ""} p-1`}>
+
+      {image && imageUrl && (<Image src={imageUrl} alt="Imagem do artigo" width={300} height={500} layout="responsive" className="cursor-pointer" onClick={handleClick} />)}
+      {title && <div className="pb-3 text-lg font-bold cursor-pointer" onClick={handleClick}>{article.title}</div>}
+      {content && <div className="line-clamp-2 text-[#919191]" onClick={handleClick}>{article.content}</div>}
+
+      {author && (
+        <div className="flex items-center justify-between mt-3">
+          <p className="text-sm text-gray-600">
+            por {article.author.name},
+            {new Date(article.releaseDate).toLocaleDateString()}
+          </p>
+          <Button className="bg-[#FF3B30]" onClick={handleClick}>LER MAIS</Button>
         </div>
-      </div>
+      )}
     </div>
+
   );
+
 };
