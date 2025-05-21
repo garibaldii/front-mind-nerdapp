45889e9fe336598dd924fe64d3f134f499f12263
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 17:17:53 -0300

    ✨feat: implement article retrieval and submission functions in ArticleService

--- src/service/ArticleService.ts
+++ src/service/ArticleService.ts
@@ -1,4 +1,6 @@
+import { IArticle } from "@/interface/IArticle"
 import { api } from "./http"
+import { IUser } from "@/interface/IUser"
 
 
 export const getArticles = async () => {
@@ -7,4 +9,35 @@ export const getArticles = async () => {
     const data = response.data
 
     return data
+}
+
+export const getArticleById = async (id: number) => {
+    const response = await api.get(`article/${id}`)
+
+    const data = response.data
+
+    return data
+}
+
+export const postArticle = async (
+    title: string,
+    content: string,
+    authorId: number,
+    image?: File | null
+) => {
+    const formData = new FormData()
+    formData.append("title", title)
+    formData.append("content", content)
+    formData.append("authorId", authorId.toString())
+    if (image) {
+        formData.append("image", image)
+    }
+
+    const response = await api.post("/article", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    })
+
+    return response.data
 }
