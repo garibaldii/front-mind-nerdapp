41a0d1240a138173fc9dd609ec086c7180817f8f
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 17:17:23 -0300

    ✨feat: refactor NavBar component to integrate user context and enhance logout functionality

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,48 +1,41 @@
-"use client";
+'use client'
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import { Button } from "../ui/button";
 
-import { jwtDecode } from "jwt-decode";
-import { useEffect, useState } from "react";
 import PictureLogo from "../../../public/PictureLogo";
+import { logout } from "@/service/UserService";
 
-type tokenPayload = {
-  email: string;
-  id: string;
-};
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useUser } from "@/context/UserContext";
 
 export const NavBar = () => {
   const router = useRouter();
+  const { user } = useUser()
 
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      try {
-        const decoded = jwtDecode<tokenPayload>(token);
-        const email = decoded.id;
-        setEmail(email);
-        setEmail(email);
-      } catch (error: any) {
-        console.error(error.data.response);
-      }
-    }
-  }, []);
+  const handleLogout = async () => {
+    await logout()
+    alert("Você foi deslogado")
+  }
 
   return (
     <div>
-<div className="flex fixed right-0 z-50 bg-white w-full items-center justify-between px-10 py-3 border-b border-gray-200">
+      <div className="flex fixed right-0 z-51 bg-white w-full items-center justify-between px-10 py-3 border-b border-gray-200">
+
         <PictureLogo />
 
         <div className="flex">
           <Button
             variant="ghost"
-            onClick={() => router.push("/pages/product")}
+            onClick={() => router.push("/pages/home")}
           >
             Home
           </Button>
@@ -56,20 +49,45 @@ export const NavBar = () => {
 
           <div className="border-l h-6 mx-4 self-center border-black" />
 
-          <div>
-            <Button
-              variant="ghost"
-              onClick={() => router.push("/")}
-            >
-              Entrar{" "}
-            </Button>
-            <Button variant="default" onClick={() => router.push("/")}>
-              Registrar{" "}
-            </Button>
-          </div>
+          {!user ? (
+            <div>
+              <Button
+                variant="ghost"
+                onClick={() => router.push("/login")}
+              >
+                Entrar
+              </Button>
+              <Button variant="default" onClick={() => router.push("/signUp")}>
+                Registrar
+              </Button>
+            </div>) : (
+
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost">Olá, {user?.name || user?.email}</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem onClick={() => router.push("/pages/profile")}>
+                      Ver perfil
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => router.push("/pages/myArticles")} >
+                      Meus Artigos
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={handleLogout}>
+                    Log out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          )}
         </div>
       </div>
-      F
+      <div className=" top-0 z-55 bg-white w-full px-10 py-10 " />
     </div>
   );
 };
