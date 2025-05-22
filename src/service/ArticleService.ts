c2365f648675c16bc88e79f8e5f5e11f22142abd
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 22:30:08 -0300

    ✨refactor: clean up ArticleService code for consistency and readability

--- src/service/ArticleService.ts
+++ src/service/ArticleService.ts
@@ -1,43 +1,75 @@
-import { IArticle } from "@/interface/IArticle"
-import { api } from "./http"
-import { IUser } from "@/interface/IUser"
-
+import { IArticle } from "@/interface/IArticle";
+import { api } from "./http";
+import { IUser } from "@/interface/IUser";
 
 export const getArticles = async () => {
-    const response = await api.get("/article")
+  const response = await api.get("/article");
 
-    const data = response.data
+  const data = response.data;
 
-    return data
-}
+  return data;
+};
 
 export const getArticleById = async (id: number) => {
-    const response = await api.get(`article/${id}`)
+  const response = await api.get(`/article/${id}`);
 
-    const data = response.data
+  const data = response.data;
 
-    return data
-}
+  console.log("artigo ", data);
+
+  return data;
+};
 
 export const postArticle = async (
-    title: string,
-    content: string,
-    authorId: number,
-    image?: File | null
+  title: string,
+  content: string,
+  authorId: number,
+  image?: File | null
 ) => {
-    const formData = new FormData()
-    formData.append("title", title)
-    formData.append("content", content)
-    formData.append("authorId", authorId.toString())
-    if (image) {
-        formData.append("image", image)
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("content", content);
+  formData.append("authorId", authorId.toString());
+  if (image) {
+    formData.append("image", image);
+  }
+
+  const response = await api.post("/article", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
+
+export const updateArticle = async (
+  id: number,
+  title: string,
+  content: string,
+  image?: File | null
+) => {
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("content", content);
+  if (image) {
+    formData.append("image", image);
+  }
+  const response = await api.put(`/article/${id}`, formData, {
+    headers: {
+        "Content-Type": "multipart/form-data"
     }
+  });
 
-    const response = await api.post("/article", formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    })
+  const data = response.data;
 
-    return response.data
+  return data;
+};
+
+export const deleteArticle = async (id: number) => {
+  const response = await api.delete(`/article/${id}`)
+
+  const data = response.data
+
+  return data
 }
