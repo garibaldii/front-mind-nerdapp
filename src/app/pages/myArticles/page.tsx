3cd8f1d2be4a57d07abfcfed1f419249614b3b81
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 22:33:40 -0300

    ♻️refactor: simplify MyArticles component structure and ensure proper rendering of ArticleDataTable

--- src/app/pages/myArticles/page.tsx
+++ src/app/pages/myArticles/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import { NavBar } from "@/components/molecules/Navbar"
+import ArticleDataTable from "@/components/ui/datatable/ArticleDataTable"
 import { useUser } from "@/context/UserContext"
 
-function MyArticles(){
-    const {user, refreshUserData} = useUser()
+function MyArticles() {
+  const { user } = useUser()
 
-    return(
-        <div>
-            <NavBar/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavBar />
+      {user && user.articles && (
+        <ArticleDataTable
+          articles={user.articles}
+        />
+      )}
+    </div>
+  )
+}   
 
 export default MyArticles
