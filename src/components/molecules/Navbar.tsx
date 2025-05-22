75ab09501a93cfd41731386859770312ec2f3b31
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 22:31:10 -0300

    ✨feat: enhance NavBar with logout modal and user profile options

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 
@@ -14,29 +14,30 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import { useUser } from "@/context/UserContext";
+import { useState } from "react";
+import { AlertModal } from "./AlertModal";
 
 export const NavBar = () => {
   const router = useRouter();
-  const { user } = useUser()
+  const { user, refreshUserData } = useUser();
+
+  const [modal, setModal] = useState(false);
 
   const handleLogout = async () => {
-    await logout()
-    alert("Você foi deslogado")
-  }
+    await logout();
+    setModal(true)
+    refreshUserData();
+  };
 
   return (
     <div>
       <div className="flex fixed right-0 z-51 bg-white w-full items-center justify-between px-10 py-3 border-b border-gray-200">
-
         <PictureLogo />
 
         <div className="flex">
-          <Button
-            variant="ghost"
-            onClick={() => router.push("/pages/home")}
-          >
+          <Button variant="ghost" onClick={() => router.push("/pages/home")}>
             Home
           </Button>
 
@@ -51,29 +52,39 @@ export const NavBar = () => {
 
           {!user ? (
             <div>
-              <Button
-                variant="ghost"
-                onClick={() => router.push("/login")}
-              >
+              <Button variant="ghost" onClick={() => router.push("/login")}>
                 Entrar
               </Button>
               <Button variant="default" onClick={() => router.push("/signUp")}>
                 Registrar
               </Button>
-            </div>) : (
-
+            </div>
+          ) : (
             <div>
+              <Button
+                variant={"ghost"}
+                onClick={() => router.push("/pages/postArticle")}
+              >
+                Publicar
+              </Button>
+
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant="ghost">Olá, {user?.name || user?.email}</Button>
+                  <Button variant="ghost">
+                    Olá, {user?.name || user?.email}
+                  </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-56">
                   <DropdownMenuSeparator />
                   <DropdownMenuGroup>
-                    <DropdownMenuItem onClick={() => router.push("/pages/profile")}>
+                    <DropdownMenuItem
+                      onClick={() => router.push("/pages/profile")}
+                    >
                       Ver perfil
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push("/pages/myArticles")} >
+                    <DropdownMenuItem
+                      onClick={() => router.push("/pages/myArticles")}
+                    >
                       Meus Artigos
                     </DropdownMenuItem>
                   </DropdownMenuGroup>
@@ -88,6 +99,15 @@ export const NavBar = () => {
         </div>
       </div>
       <div className=" top-0 z-55 bg-white w-full px-10 py-10 " />
+
+      {modal && (
+        <AlertModal
+          onClose={() => setModal(false)}
+          title={"Atenção"}
+          description={"Você foi deslogado"}
+          open={modal}
+        />
+      )}
     </div>
   );
 };
