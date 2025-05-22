06c4ee4f4c3020b8aa5976de805f5e3e04b2e600
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 22:32:42 -0300

    ♻️refactor: improve code formatting and consistency in FeaturedArticle component

--- src/components/molecules/FeaturedArticle.tsx
+++ src/components/molecules/FeaturedArticle.tsx
@@ -9,32 +9,64 @@ import { useEffect, useState } from "react";
 
 import { useImageUrl } from "@/hooks/useImageUrl";
 
-
 type Props = {
   article: IArticle;
-  title: boolean
-  content: boolean
-  author: boolean
-  image: boolean
-  hoverEffect: boolean
+  title: boolean;
+  content: boolean;
+  author: boolean;
+  image: boolean;
+  hoverEffect: boolean;
 };
 
-export const FeaturedArticle = ({ article, title, content, author, image, hoverEffect }: Props) => {
+export const FeaturedArticle = ({
+  article,
+  title,
+  content,
+  author,
+  image,
+  hoverEffect,
+}: Props) => {
   const router = useRouter();
 
-  const imageUrl = useImageUrl(article.image?.data, image)
+  const imageUrl = useImageUrl(article.image?.data, image);
 
   const handleClick = () => {
     router.push(`/pages/article/${article.id}`); //redireciona o usuário ao artigo clicado via id = "pages/article/[id]"
   };
 
   return (
-    <div className={`transition-colors duration-300 rounded-lg 
-  ${hoverEffect ? "hover:bg-gray-700 cursor-pointer" : ""} p-1`}>
-
-      {image && imageUrl && (<Image src={imageUrl} alt="Imagem do artigo" width={300} height={500} layout="responsive" className="cursor-pointer" onClick={handleClick} />)}
-      {title && <div className="pb-3 text-lg font-bold cursor-pointer" onClick={handleClick}>{article.title}</div>}
-      {content && <div className="line-clamp-2 text-[#919191]" onClick={handleClick}>{article.content}</div>}
+    <div
+      className={`transition-colors duration-300 rounded-lg 
+  ${hoverEffect ? "hover:bg-gray-700 cursor-pointer" : ""} p-1`}
+    >
+      {image && imageUrl && (
+        <Image
+          src={imageUrl}
+          alt="Imagem do artigo"
+          width={300}
+          height={500}
+          style={{
+            width: "100%", // força largura fixa
+            height: "400px", // força altura fixa
+            objectFit: "cover", // cobre toda a área sem distorcer, cortando se necessário
+          }}
+          className="cursor-pointer"
+          onClick={handleClick}
+        />
+      )}
+      {title && (
+        <div
+          className="pb-3 text-lg font-bold cursor-pointer"
+          onClick={handleClick}
+        >
+          {article.title}
+        </div>
+      )}
+      {content && (
+        <div className="line-clamp-2 text-[#919191]" onClick={handleClick}>
+          {article.content}
+        </div>
+      )}
 
       {author && (
         <div className="flex items-center justify-between mt-3">
@@ -42,11 +74,11 @@ export const FeaturedArticle = ({ article, title, content, author, image, hoverE
             por {article.author.name},
             {new Date(article.releaseDate).toLocaleDateString()}
           </p>
-          <Button className="bg-[#FF3B30]" onClick={handleClick}>LER MAIS</Button>
+          <Button className="bg-[#FF3B30]" onClick={handleClick}>
+            LER MAIS
+          </Button>
         </div>
       )}
     </div>
-
   );
-
 };
