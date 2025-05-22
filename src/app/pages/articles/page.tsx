a847634f4f5160149b10fcd87130c75868993918
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-21 22:33:22 -0300

    ♻️refactor: improve code formatting and consistency in Articles component

--- src/app/pages/articles/page.tsx
+++ src/app/pages/articles/page.tsx
@@ -1,37 +1,37 @@
-'use client'
+"use client";
 
-import { FeaturedArticle } from "@/components/molecules/FeaturedArticle"
-import { NavBar } from "@/components/molecules/Navbar"
-import { useArticle } from "@/context/ArticleContext"
-import { useEffect } from "react"
+import { FeaturedArticle } from "@/components/molecules/FeaturedArticle";
+import { NavBar } from "@/components/molecules/Navbar";
+import { useArticle } from "@/context/ArticleContext";
+import { useEffect } from "react";
 
 function Articles() {
+  const { articles, refreshArticleData } = useArticle();
 
-    const { articles } = useArticle()
+  useEffect(() => {
+    console.log("estou rodando");
+    refreshArticleData();
+  }, [refreshArticleData]);
 
-    useEffect(() => {
-        console.log("Mudanca em articles detectada! Atualizando página")
-    }, [articles])
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
 
-    return (
-        <div className="min-h-screen flex flex-col">
-            <NavBar />
-
-            <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
-                {articles.map((article, index) => (
-                    <FeaturedArticle
-                        key={index}
-                        article={article}
-                        title={true}
-                        content={true}
-                        author={true}
-                        image={true}
-                        hoverEffect={false}
-                    />
-                ))}
-            </main>
-        </div>
-    )
+      <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
+        {articles.map((article, index) => (
+          <FeaturedArticle
+            key={index}
+            article={article}
+            title={true}
+            content={true}
+            author={true}
+            image={true}
+            hoverEffect={false}
+          />
+        ))}
+      </main>
+    </div>
+  );
 }
 
-export default Articles
+export default Articles;
